a65592903e8cc64beb44410c2fd389de12a3b6b1
tera1707 <Masa1017>
2024-11-16 14:19:52 +0900

    コミット忘れ修正

---
+++ WindowMessageSender/WindowMessageSender/IntToHexStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace WindowMessageSender;
+
+public class IntToHexStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        if (value is int v && parameter is string keta)
+        {
+            return v.ToString($"X{keta}");
+        }
+        return "";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
